212b15f9da17ba1811f258f4b9eb2685eb188a3d
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-08 17:37:36 -0500

    Create componenet for monaco code editor

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MonacoEditor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+    return (
+        <div>
+            <MonacoEditor />
+        </div>
+    )
+}
+
+export default CodeEditor;
