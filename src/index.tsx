debacc648b463d6cadfd06ebc03f5e7c48ff5068
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-08 17:12:20 -0500

    Add iframe to receive bundled and transpiled code to execute

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState(''); 
-    const [code, setCode] = useState(''); 
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -27,6 +27,8 @@ const App = () => {
     const handleClick = async () => {
         if(!ref.current) return; 
         
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'], 
             bundle: true, 
@@ -41,8 +43,28 @@ const App = () => {
             }
         });
 
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
+
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        try {
+                            eval(event.data);
+                        } catch (error) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
+                            console.error(error);
+                        }
+                    }, false);
+                </script>
+            </body>
+        </html>
+    `;
 
     return (
         <div>
@@ -50,9 +72,9 @@ const App = () => {
             <div>
                 <button onClick={handleClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
-    )
-}
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'))
