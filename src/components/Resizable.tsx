628f4541baa234b2f9a699287dbbf08b9a0a7d04
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-09 15:41:28 -0500

    Add a min and max constraint for resizing the CodeCell component

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -6,11 +6,16 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-    return <ResizableBox 
-    height={300} 
-    width={Infinity} // react-resizable doesn't take percentage and we want it to take up as much width as it can
-    resizeHandles={['s']}
-    >{children}</ResizableBox>
+    return (
+    <ResizableBox 
+        minConstraints={[Infinity, 40]}
+        maxConstraints={[Infinity, window.innerHeight * 0.9]}
+        height={300} 
+        width={Infinity} // react-resizable doesn't take percentage and we want it to take up as much width as it can
+        resizeHandles={['s']}
+        >{children}
+    </ResizableBox>
+    );
 };
 
 export default Resizable;
