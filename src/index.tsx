b8eb6059801b51275997d12fe25fd17826931f7d
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-09 13:27:53 -0500

    Move bundle code to its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,21 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import './main.css';
-import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react'; 
 import ReactDOM from 'react-dom'; 
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import JavaScriptCodeEditor from './components/JavaScriptCodeEditor';
 import RubyCodeEditor from './components/RubyCodeEditor';
 import ElixirCodeEditor from './components/ElixirCodeEditor';
 import Preview from './components/Preview';
+import bundle from './bundler'
 
 const App = () => {
-    const ref = useRef<any>();
   
     const [input, setInput] = useState(''); 
     const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true, 
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
-
-    useEffect(() => {
-        startService();
-    }, []);
-
     const handleClick = async () => {
-        if(!ref.current) return; 
-        
-        const result = await ref.current.build({
-            entryPoints: ['index.js'], 
-            bundle: true, 
-            write: false, 
-            plugins: [
-                unpkgPathPlugin(),
-                fetchPlugin(input)
-            ],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundle(input);
+        setCode(output);
     };
 
     return (
