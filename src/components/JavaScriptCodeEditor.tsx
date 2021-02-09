158d3785ee5dd375e7f9253ae00332f34f8b2fd8
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-08 20:48:35 -0500

    Add event listener to listen for changes in the editor and update the state using onChange passed from the parent component

--- src/components/JavaScriptCodeEditor.tsx
+++ src/components/JavaScriptCodeEditor.tsx
@@ -6,23 +6,31 @@ interface CodeEditorProps {
     onChange(value: string): void;
 }
 
-const JavaScriptCodeEditor:React.FC<CodeEditorProps> = ({ initialValue }) => {
+const JavaScriptCodeEditor:React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+
+    const handleEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+        monacoEditor.onDidChangeModelContent(() => {
+            onChange(getValue());
+        });
+    }
+
     return (
         <div>
             <MonacoEditor 
-            height="500px"    
-            value={initialValue}        
-            language="javascript" 
-            theme="dark" 
-            options={{
-                wordWrap: 'on',
-                minimap: { enabled: false },
-                showUnused: false,
-                folding: false,
-                lineNumbersMinChars: 3,
-                fontSize: 24,
-                scrollBeyondLastLine: false,
-                automaticLayout: true
+                height="500px"   
+                editorDidMount={handleEditorDidMount} 
+                value={initialValue}        
+                language="javascript" 
+                theme="dark" 
+                options={{
+                    wordWrap: 'on',
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 24,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true
             }}/>
         </div>
     )
