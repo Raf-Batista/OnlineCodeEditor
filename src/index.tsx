200564f1bd7ceb94b479dcf0a1fa41ce2da18c3f
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-09 13:16:59 -0500

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,13 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 import JavaScriptCodeEditor from './components/JavaScriptCodeEditor';
 import RubyCodeEditor from './components/RubyCodeEditor';
 import ElixirCodeEditor from './components/ElixirCodeEditor';
+import Preview from './components/Preview';
 
 const App = () => {
     const ref = useRef<any>();
-    const iframe = useRef<any>();
+  
     const [input, setInput] = useState(''); 
+    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -25,15 +27,9 @@ const App = () => {
         startService();
     }, []);
 
-    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInput(event.target.value);
-    };
-
     const handleClick = async () => {
         if(!ref.current) return; 
         
-        iframe.current.srcdoc = html;
-
         const result = await ref.current.build({
             entryPoints: ['index.js'], 
             bundle: true, 
@@ -48,28 +44,8 @@ const App = () => {
             }
         });
 
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+        setCode(result.outputFiles[0].text);
     };
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try {
-                            eval(event.data);
-                        } catch (error) {
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
-                            console.error(error);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
 
     return (
         <div>
@@ -77,11 +53,10 @@ const App = () => {
                 initialValue="const a = 1;"
                 onChange={(value) => setInput(value)}
             />
-            <textarea onChange={handleChange}></textarea>
             <div>
                 <button onClick={handleClick}>Submit</button>
             </div>
-            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+            <Preview code={code}/>
             {/* <RubyCodeEditor />
             <ElixirCodeEditor /> */}
         </div>
