ca5f2a80d80b975b5841fdb323c3479eca16eb75
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-09 11:53:58 -0500

    Import bulma css theme and main css

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import './main.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react'; 
 import ReactDOM from 'react-dom'; 
