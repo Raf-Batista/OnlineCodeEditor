6cc20737b9cc4a478a0ebd61ea3297f13aab817b
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-09 22:32:10 -0500

    Update

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -3,36 +3,37 @@ import MDEditor from '@uiw/react-md-editor';
 import './TextEditor.css';
 
 const TextEditor: React.FC = () => {
+    const ref = useRef<HTMLDivElement | null>(null);
     const [editing, setEditing] = useState(false);
-    const ref = useRef<HTMLDivElement | null>(null)
+    const [value, setValue] = useState('# Header');
 
     useEffect(() => {
         const listener = (event: MouseEvent) => {
             if (ref.current && event.target && ref.current.contains(event.target as Node)) {
                 return;
             }
-        
+
             setEditing(false);
         };
 
         document.addEventListener('click', listener, { capture: true });
 
         return () => {
-            document.removeEventListener('click', listener, { capture: true })
+            document.removeEventListener('click', listener, { capture: true });
         };
     }, []);
 
     if (editing) {
         return (
-            <div>
-                <MDEditor />
+            <div ref={ref}>
+                <MDEditor value={value} onChange={ (value) => setValue(value || '') } />
             </div>
         );
     };
 
     return (
-        <div className="text-editor" ref={ref} onClick={() => setEditing(true)}>
-            <MDEditor.Markdown source={'# Header'} />
+        <div className="text-editor" onClick={() => setEditing(true)}>
+            <MDEditor.Markdown  source={value} />
         </div>
     )
 }
