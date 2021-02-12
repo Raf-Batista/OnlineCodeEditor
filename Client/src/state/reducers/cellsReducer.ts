ac74f55266d578cd83d6d33beb92fc42ea625aaa
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-12 16:58:47 -0500

    Redux setup

---
+++ Client/src/state/reducers/cellsReducer.ts
@@ -0,0 +1,55 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell'; 
+import produce from 'immer';
+import { act } from 'react-dom/test-utils';
+
+interface CellsState {
+    loading: boolean;
+    error: string | null;
+    order: string[];
+    data: {
+        [key: string]: Cell
+    }
+}
+
+const initialState: CellsState = {
+    loading: false,
+    error: null,
+    order: [],
+    data: {}
+};
+
+const reducer = produce((state: CellsState = initialState, action: Action) => {
+    switch (action.type) {
+        case ActionType.UPDATE_CELL:
+            const { id, content } = action.payload;
+            state.data[id].content = content;
+            
+            return;
+        case ActionType.DELETE_CELL:
+            delete state.data[action.payload]
+            state.order = state.order.filter(id => id !== action.payload);
+            
+            return;
+        case ActionType.MOVE_CELL:
+            const { direction } = action.payload;
+            const index = state.order.findIndex((id) => id === action.payload.id); 
+            const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+            if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+                return;
+            }
+
+            state.order[index] = state.order[targetIndex];
+            state.order[targetIndex] = action.payload.id;
+
+            return state;
+        case ActionType.INSERT_CELL_BEFORE:
+            return state;
+        default: 
+            return state;
+    }
+});
+
+export default reducer;
