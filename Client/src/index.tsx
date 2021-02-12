2940b76b90c4df61509523d1cc9294de62c55304
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-12 16:57:54 -0500

    Redux setup

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,16 +1,20 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import './main.css';
 import ReactDOM from 'react-dom'; 
+import { Provider } from 'react-redux';
+import { store } from './state';
 //import CodeCell from './components/CodeCell';
 import TextEditor from './components/TextEditor';
 
 const App = () => {
   
     return (
-        <div>
-            <TextEditor />
-        </div>
+        <Provider store={store({})}>
+            <div>
+                <TextEditor />
+            </div>
+        </Provider>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
