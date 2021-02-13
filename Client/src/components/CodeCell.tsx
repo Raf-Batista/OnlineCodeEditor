5aa11aea2dfef383779846c2432b701a72d49d20
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-13 16:46:00 -0500

    Update component to use Redux state

--- Client/src/components/CodeCell.tsx
+++ Client/src/components/CodeCell.tsx
@@ -5,15 +5,21 @@ import ElixirCodeEditor from './ElixirCodeEditor';
 import Preview from './Preview';
 import bundle from '../bundler'
 import Resizable from './Resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/useActions';
 
-const CodeCell = () => {
-    const [input, setInput] = useState(''); 
+interface CodeCellProps {
+    cell: Cell
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const [code, setCode] = useState('');
     const [error, setError] = useState('initialState');
+    const { updateCell } = useActions();
 
     useEffect(() => {
         const timer = setTimeout(async () => {
-            const output = await bundle(input);
+            const output = await bundle(cell.content);
             setCode(output.code);
             setError(output.error);
         }, 1000);
@@ -21,15 +27,15 @@ const CodeCell = () => {
         return () => {
             clearTimeout(timer);
         };
-    }, [input])
+    }, [cell.content])
 
     return (
         <Resizable direction="vertical">
             <div style={{ height: '100%', display: 'flex', flexDirection: 'row'}}>
                 <Resizable direction="horizontal">
                     <JavaScriptCodeEditor 
-                        initialValue="const a = 1;"
-                        onChange={(value) => setInput(value)}
+                        initialValue={cell.content}
+                        onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
                 <Preview code={code} error={error} />
