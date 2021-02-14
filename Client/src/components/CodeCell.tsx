2845cd90380aa23ed0b57bf6decac693d5890490
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-14 17:00:00 -0500

    Handle issue with the preview window flashing on refresh and ignore eslint warning from dependencies in useEffect hook

--- Client/src/components/CodeCell.tsx
+++ Client/src/components/CodeCell.tsx
@@ -17,6 +17,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle(cell.id, cell.content);
+            return;
+        };
+
         const timer = setTimeout(async () => {  
             createBundle(cell.id, cell.content)
         }, 1000);
@@ -24,6 +29,8 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         return () => {
             clearTimeout(timer);
         };
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [cell.content, cell.id, createBundle]);
 
     return (
