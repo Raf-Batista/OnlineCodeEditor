210b816eee3ce22285439d2fdcb1a3c5e0a28a56
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-14 17:33:09 -0500

    Add loading progress bar

--- Client/src/components/CodeCell.tsx
+++ Client/src/components/CodeCell.tsx
@@ -1,3 +1,4 @@
+import './CodeCell.css';
 import { useEffect } from 'react'; 
 import JavaScriptCodeEditor from './JavaScriptCodeEditor';
 import RubyCodeEditor from './RubyCodeEditor';
@@ -42,7 +43,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {bundle && <Preview code={bundle.code} error={bundle.error} />}
+             
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading 
+                    ? 
+                    <div className="progress-cover">
+                        <progress className="progress is-small is-primary" max="100">
+                        Loading
+                        </progress>
+                    </div>            
+                    : <Preview code={bundle.code} error={bundle.error} />
+                    }
+                </div>
                 {/* <RubyCodeEditor />
                 <ElixirCodeEditor /> */}
             </div>
