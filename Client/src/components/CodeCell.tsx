0ac8fb9ed972db230aea38c43fd9b07d63e3db75
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-14 18:54:14 -0500

    Update component to bundle code from all cells

--- Client/src/components/CodeCell.tsx
+++ Client/src/components/CodeCell.tsx
@@ -16,15 +16,30 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useTypedSelector((state) => {
+        const { data, order } = state.cells;
+        const orderedCells = order.map((id) => data[id]);
+
+        const cumulativeCode = []; 
+        for (let c of orderedCells) {
+            if (c.type === 'javascript') {
+                cumulativeCode.push(c.content);
+            }; 
+            if (c.id === cell.id) {
+                break;
+            }
+        }
+        return cumulativeCode;
+    });
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join('\n'));
             return;
         };
 
         const timer = setTimeout(async () => {  
-            createBundle(cell.id, cell.content)
+            createBundle(cell.id, cumulativeCode.join('\n'))
         }, 1000);
 
         return () => {
@@ -32,7 +47,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         };
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cell.content, cell.id, createBundle]);
+    }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
     return (
         <Resizable direction="vertical">
