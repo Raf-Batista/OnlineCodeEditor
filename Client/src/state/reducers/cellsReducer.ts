aac0810d787c4b14e70285f73c3d34738cac64f5
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-14 14:20:59 -0500

    Fix css issue that shows the buttons to add a cell whenever a user creates a new cell instead of hiding it

--- Client/src/state/reducers/cellsReducer.ts
+++ Client/src/state/reducers/cellsReducer.ts
@@ -2,7 +2,6 @@ import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell'; 
 import produce from 'immer';
-import { act } from 'react-dom/test-utils';
 
 interface CellsState {
     loading: boolean;
@@ -47,7 +46,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
             state.order[targetIndex] = action.payload.id;
 
             return state;
-        case ActionType.INSERT_CELL_BEFORE:
+        case ActionType.INSERT_CELL_AFTER:
             const cell: Cell = {
                 content: '',
                 type: action.payload.type,
@@ -59,9 +58,9 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
             const foundIndex = state.order.findIndex(id => id === action.payload.id);
 
             if (foundIndex < 0) {
-                state.order.push(cell.id);
+                state.order.unshift(cell.id);
             } else {
-                state.order.splice(foundIndex, 0, cell.id);
+                state.order.splice(foundIndex + 1, 0, cell.id);
             }
 
             return state;
