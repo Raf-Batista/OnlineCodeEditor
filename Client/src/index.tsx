1d266c51ab5cc7b255976e5421fe77ed85503f4b
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-15 21:35:06 -0500

    Import Navigation

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -6,13 +6,15 @@ import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/CellList';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
+import { useEffect } from 'react';
 
 
 const App = () => {
-  
     return (
         <Provider store={store({})}>
             <div>
+                <Navigation />
                 <Header />
                 <CellList />
             </div>
