a753abdef2cec54ca80fa63d58493d7e9df43373
Rafael Batista <RafaelB2424@gmail.com>
2021-02-19 19:21:59 -0500

    Create codes model

--- Servers/online-code-editor-rails/db/migrate/20210219231918_create_codes.rb
+++ Servers/online-code-editor-rails/db/migrate/20210219231918_create_codes.rb
@@ -1,6 +1,9 @@
 class CreateCodes < ActiveRecord::Migration[6.1]
   def change
     create_table :codes do |t|
+      t.string :title
+      t.string :order, array: true, default: []
+      t.json :data
 
       t.timestamps
     end
