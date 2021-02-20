57c309566b628faf35c09b08272c094180f95682
Rafael Batista <RafaelB2424@gmail.com>
2021-02-20 17:37:40 -0500

    Update to use correct state

--- Client/src/state/reducers/userReducer.ts
+++ Client/src/state/reducers/userReducer.ts
@@ -4,25 +4,24 @@ import { Action } from '../actions';
 
 interface UserState {
     username: string;
-    userCode: {
-        order: string[],
-        data: {}
-    }
+    codes: any
 }
 
 const initialState = {
     username: '',
-    userCode: {
-        order: [], 
-        data: {}
-    }
+    codes: [{title: '', order: [''], data: ['']}]
 };
 
 const userReducer = produce((state: UserState = initialState, action: Action) => {
     switch (action.type) {
+        case ActionType.CHECK_IF_LOGGED_IN: 
+            state.username = action.payload.username;
+            state.codes = action.payload.codes;
+
+            return state;
         case ActionType.LOGIN_USER_COMPLETE: 
             state.username = action.payload.username;
-            state.userCode = action.payload.userCode;
+            state.codes = action.payload.codes;
             
             return state;
         default: 
