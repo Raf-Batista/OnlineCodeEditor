311bc2cd5500e58ff9389d4691a5e6bfa32fbaf8
Rafael Batista <RafaelB2424@gmail.com>
2021-02-20 17:31:24 -0500

    Fix syntax error

--- Servers/online-code-editor-rails/spec/requests/ruby_scripts_request_spec.rb
+++ Servers/online-code-editor-rails/spec/requests/ruby_scripts_request_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe "RubyScripts", type: :request do
 
         json_response = JSON.parse(response.body)
 
-        exxpect(json_response).to eq(2)
+        expect(json_response).to eq(2)
     end 
 end
