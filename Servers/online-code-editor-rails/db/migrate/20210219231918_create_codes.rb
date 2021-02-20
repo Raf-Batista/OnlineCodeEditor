84d315eec82b3aff43ff558040a895d957220eba
Rafael Batista <RafaelB2424@gmail.com>
2021-02-20 17:31:48 -0500

    Add user id field

--- Servers/online-code-editor-rails/db/migrate/20210219231918_create_codes.rb
+++ Servers/online-code-editor-rails/db/migrate/20210219231918_create_codes.rb
@@ -1,6 +1,7 @@
 class CreateCodes < ActiveRecord::Migration[6.1]
   def change
     create_table :codes do |t|
+      t.integer :user_id
       t.string :title
       t.string :order, array: true, default: []
       t.json :data
