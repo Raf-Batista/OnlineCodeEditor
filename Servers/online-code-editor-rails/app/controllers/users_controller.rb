db7423686b464e4b23c08176e5420839a5a22596
Rafael Batista <RafaelB2424@gmail.com>
2021-02-19 19:20:59 -0500

    Add create action to create a new user

--- Servers/online-code-editor-rails/app/controllers/users_controller.rb
+++ Servers/online-code-editor-rails/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def create 
+        user = User.new(users_params)
+
+        if user.valid? 
+            user.save 
+            render json: user, status: :created and return
+        end 
+
+        render json: { errors: user.errors.full_messages} and return
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :password)
+    end 
 end
