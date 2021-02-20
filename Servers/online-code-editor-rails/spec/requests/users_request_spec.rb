f6f162957d336ace863419d259c8af1f7058215a
Rafael Batista <RafaelB2424@gmail.com>
2021-02-19 19:24:50 -0500

    Add test to see if a user can be created

--- Servers/online-code-editor-rails/spec/requests/users_request_spec.rb
+++ Servers/online-code-editor-rails/spec/requests/users_request_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+    it "can successfully create a user" do 
+        post "/users", params: {user: {username: "Test", password: "password123"}}
 
+        expect(User.all.size).to eq(1)
+    end 
 end
