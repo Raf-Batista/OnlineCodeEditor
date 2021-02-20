983eb36c1c3b44c536bc32d8b9187caf3010fa05
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2021-02-20 00:29:12 -0500

    Add userReducer to rootReducers

--- Client/src/state/reducers/index.ts
+++ Client/src/state/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cellsReducer from './cellsReducer';
 import bundlesReducer from './bundlesReducer';
+import userReducer from './userReducer';
 
 const rootReducers = combineReducers({
     cells: cellsReducer,
-    bundles: bundlesReducer
+    bundles: bundlesReducer,
+    user: userReducer
 });
 
 export default rootReducers;
