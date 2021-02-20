4d3c05d8ac0c71bc334318ad7a06f865e19386f2
Rafael Batista <RafaelB2424@gmail.com>
2021-02-19 19:23:01 -0500

    Add routes for users, codes, and to execute code

--- Servers/online-code-editor-rails/config/routes.rb
+++ Servers/online-code-editor-rails/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/execute', to: 'ruby_scripts#execute'
+
+  resources :users do 
+    resources :codes, only: [:create]
+  end 
 end
