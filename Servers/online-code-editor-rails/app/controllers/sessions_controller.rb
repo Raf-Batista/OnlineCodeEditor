b64cbde591834d552772a770f03f041f1cb8f921
Rafael Batista <RafaelB2424@gmail.com>
2021-02-19 19:20:32 -0500

    Add action to log a user in

--- Servers/online-code-editor-rails/app/controllers/sessions_controller.rb
+++ Servers/online-code-editor-rails/app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def create 
+        user = User.find_by(:username)
+
+        if user.authenticate(:password) then render json: user, status: :ok and return end
+        
+        render json: {errors: 'username or password incorrect'} and return
+        
+    end 
+
+    private 
+
+    def user_params 
+        params.permit(:username. :password)
+    end 
 end
