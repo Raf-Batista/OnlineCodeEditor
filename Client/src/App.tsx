1bc1946848348f165c2aa9b4d3207181073e463b
Rafael Batista <RafaelB2424@gmail.com>
2021-02-21 17:11:14 -0500

    Refactor

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,25 +1,23 @@
-import { Provider } from 'react-redux';
-import { store } from './state';
-import { Header, Navigation, CellList, Signup, Login  } from './components';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from "./state";
+import { Header, Navigation, CellList, Signup, Login } from "./components";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-    return (
-        <Provider store={store({})}>
-            <Router>
-                <Navigation />
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={CellList} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/login" component={Login} />
-                </Switch>
-            </Router>
-        </Provider>
-    );
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={CellList} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </>
+  );
 };
 
-export default App
+export default App;
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
