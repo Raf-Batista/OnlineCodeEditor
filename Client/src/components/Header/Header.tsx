67c9aa7b4dc58502ea78959d24064984459479d6
Rafael Batista <RafaelB2424@gmail.com>
2021-02-21 18:00:06 -0500

    Add help button

--- Client/src/components/Header/Header.tsx
+++ Client/src/components/Header/Header.tsx
@@ -4,6 +4,9 @@ const Header = () => {
     return (
         <div className="header">
             <h1>Code and Markdown Editor</h1>
+            <div className="header-help button is-primary">
+                <span>Help</span>
+            </div>
         </div>
     )
 };
