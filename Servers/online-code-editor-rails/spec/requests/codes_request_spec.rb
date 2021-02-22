89315139cfa9d95e5f41639a70ba9b0186d95a9d
Raf-Batista <RafaelB2424@gmail.com>
2021-02-21 21:47:56 -0800

    Add test to check if a user can successfully save code

--- Servers/online-code-editor-rails/spec/requests/codes_request_spec.rb
+++ Servers/online-code-editor-rails/spec/requests/codes_request_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Codes", type: :request do
+  it "can save a user's code" do 
+    user = User.create(username: 'test', password: 'password123')
 
+    params = {
+      user_id: user.id,
+      code: {
+        title: 'test',
+        order: ['a3gd', '33ga'],
+        data: {
+          uk78k: {
+            content: '#Test',
+            type: 'text',
+            id: 'uk78k'
+          },
+          bszat: {
+            content: 'const a = 1;',
+            type: 'javascript',
+            id: 'bszat'
+          }
+        }
+      }
+    }
+
+    post "/users/#{user.id}/codes", params: params
+     
+    expect(Code.all.size).to eq(1)
+  end 
 end
