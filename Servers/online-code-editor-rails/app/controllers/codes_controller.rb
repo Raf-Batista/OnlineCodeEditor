5232575e7891cd40e84b2c4671e8229bd20ebe31
Raf-Batista <RafaelB2424@gmail.com>
2021-02-21 21:49:28 -0800

    Fix logic error in code_params and render a user

--- Servers/online-code-editor-rails/app/controllers/codes_controller.rb
+++ Servers/online-code-editor-rails/app/controllers/codes_controller.rb
@@ -1,15 +1,16 @@
 class CodesController < ApplicationController
     def create 
-        user = User.find_by(username: params[:username])
+        user = User.find_by(id: params[:user_id])
         if user then user.codes.build(code_params) end 
-        if user.save then render json: 'Created', status: :created and return end
 
-        render json: {errors: 'Failed to save code'} and return end
+        if user.save then render json: {id: user.id, username: user.username, codes: user.codes}, status: :created and return end
 
-        private 
+        render json: {errors: 'Failed to save code'} and return 
+    end 
 
-        def code_params 
-            params.require(:code).permit(:title, order: [], date: {})
-        end 
+    private 
+
+    def code_params 
+        params.require(:code).permit(:title, order: [], data: {})
     end 
 end
