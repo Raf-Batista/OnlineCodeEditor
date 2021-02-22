f1ba5543ea784e41f83e06e1cb06dd34deb479d9
Raf-Batista <RafaelB2424@gmail.com>
2021-02-21 21:48:59 -0800

    Update controllers to render a user's id

--- Servers/online-code-editor-rails/app/controllers/sessions_controller.rb
+++ Servers/online-code-editor-rails/app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def create 
         user = User.find_by(username: params[:username])
 
-        if user.authenticate(params[:password]) then render json: {username: user.username, codes: user.codes}, status: :ok and return end
+        if user.authenticate(params[:password]) then render json: {username: user.username, id: user.id, codes: user.codes}, status: :ok and return end
         
         render json: {errors: 'username or password incorrect'} and return
         
--- Servers/online-code-editor-rails/app/controllers/users_controller.rb
+++ Servers/online-code-editor-rails/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.valid? 
             user.save 
-            render json: {username: user.username, codes: user.codes}, status: :created and return
+            render json: {id: user.id, username: user.username, codes: user.codes}, status: :created and return
         end 
 
         render json: { errors: user.errors.full_messages } and return
