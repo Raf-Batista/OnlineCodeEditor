745b27760dd2dd547291ad59837741befefdc27c
Raf-Batista <RafaelB2424@gmail.com>
2021-02-21 21:52:47 -0800

    Fix infinite loop bug which would crash the browser

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,31 +1,40 @@
 //@ts-nocheck
 import React, { useState, useEffect } from 'react';
-import { Header, Navigation, CellList, Signup, Login } from "./components";
+import { Header, Navigation, CellList, Signup, Login, UserCodes } from "./components";
 import { Route, Switch } from "react-router-dom";
 import { useActions } from './hooks/useActions';
 import { useTypedSelector } from './hooks/useTypedSelector';
+import logger from './state/middleware/logger';
 
 const App: React.FC = () => {
     const [loggedIn, setLoggedIn] = useState(false);
-    const { checkIfLoggedIn } = useActions();
+    const { checkIfLoggedIn, saveCode, loadCode } = useActions();
     const loggedInUser = useTypedSelector((state) => state.user);
+    const {order, data} = useTypedSelector((state) => state.cells);
+    const userCode = {order: order, data: data};
+
+    // Refactor this 
+    const user = localStorage.getItem('user');
+    const userId = user && JSON.parse(user).id
+
     useEffect(() => {
-      const user = localStorage.getItem("user");
-      if (user) {
+      const user = localStorage.getItem('user');
+      if (user && !loggedIn) {
           setLoggedIn(true);
           checkIfLoggedIn(JSON.parse(user));
-      } else if (loggedInUser.username !== '') {
-        setLoggedIn(true);
-      }
-    }, [checkIfLoggedIn, loggedInUser]);
+      } 
+    }, [checkIfLoggedIn, loggedInUser, loggedIn]);
+
+
   return (
     <>
-      <Navigation loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Navigation loggedIn={loggedIn} setLoggedIn={setLoggedIn} save={saveCode} load={loadCode} userCode={userCode} userId={userId} />
       <Header />
       <Switch>
         <Route exact path="/" component={CellList} />
         <Route exact path="/signup" render={(routeProps) => <Signup {...routeProps} loggedIn={loggedIn} /> } />
         <Route exact path="/login" render={(routeProps) => <Login {...routeProps} loggedIn={loggedIn} /> } />
+        <Route exact path="/codes" render={(routeProps) => <UserCodes {...routeProps}  user={loggedInUser} load={loadCode} /> } />
       </Switch>
     </>
   );
