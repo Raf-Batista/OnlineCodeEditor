38028f767c652fecec9189c2b66202717d32f0b4
Raf-Batista <RafaelB2424@gmail.com>
2021-02-23 12:19:03 -0800

    Refactor

--- Client/src/components/Navigation/Navigation.tsx
+++ Client/src/components/Navigation/Navigation.tsx
@@ -1,34 +1,20 @@
 // @ts-nocheck
 import "./Navigation.css";
-import { useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { SyntheticEvent } from "react";
-import { useActions } from "../../hooks/useActions";
-import { useTypedSelector } from "../../hooks/useTypedSelector";
+
 
 // interface NavigationProps {
 //   user: string
 // }
 
-const Navigation: React.FC<NavigationProps> = ({ loggedIn, setLoggedIn, save, load, userCode, userId }) => {
+const Navigation: React.FC<NavigationProps> = ({ loggedIn, setLoggedIn }) => {
   const handleClick = (e: SyntheticEvent) => {
     const toggler = document.querySelector("input");
 
     if (toggler?.checked === true) {
       toggler.checked = false;
     }
-  };
-
-  const handleSave = () => {
-    const toggler = document.querySelector("input");
-
-    if (toggler?.checked === true) {
-      toggler.checked = false;
-    }
-    
-    const code = {...userCode, title: Date.now()};
-
-    save(code, userId);
   };
 
   const handleLogout = (e: SyntheticEvent) => {
@@ -57,11 +43,13 @@ const Navigation: React.FC<NavigationProps> = ({ loggedIn, setLoggedIn, save, lo
                 </li>
               {loggedIn ? (
                 <>
-                  <li onClick={handleSave}>Save</li>
+                  <li onClick={handleClick}>
+                    <Link to="/save">Save</Link>
+                  </li>
                   <li onClick={handleClick}>
                     <Link to="/codes">Load</Link>
                   </li>
-                  <li>Email</li>
+
                   <li onClick={handleLogout}>
                     <Link to="/">Logout</Link>
                   </li>
