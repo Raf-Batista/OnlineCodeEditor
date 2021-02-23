c8d2c20b5f0d9ad2c4748eeaf42d09d2a18b5048
Raf-Batista <RafaelB2424@gmail.com>
2021-02-23 15:49:47 -0800

    Add header letting user know they are on viewing their saved code

--- Client/src/components/UserCodes/UserCodes.tsx
+++ Client/src/components/UserCodes/UserCodes.tsx
@@ -10,6 +10,7 @@ interface UserCodesProps {
 const UserCodes: React.FC<UserCodesProps> = ({ user, load, history }) => {
     return (
         <div className="user-codes">
+            <h2>My Saved Code</h2>
             {user.codes && user.codes.map((code) => <div key={code.id}><Code code={code} load={load} history={history} /></div>)} 
         </div>
     )
