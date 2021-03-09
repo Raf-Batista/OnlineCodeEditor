2507b3014def924900c9cab9100cc05790f0198b
Rafael Batista <RafaelB2424@gmail.com>
2021-03-09 15:28:54 -0500

    Update cors to use env variable or localhost

--- Servers/online-code-editor-rails/config/initializers/cors.rb
+++ Servers/online-code-editor-rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["CLIENT"]
+    origins  ENV["CLIENT"] || 'http://localhost:5000' # using env variable alone lead to Cors error
 
     resource '*',
       headers: :any,
