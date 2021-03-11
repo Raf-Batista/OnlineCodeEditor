767814ee4a1d7de0a95f7e73fff1313cb8231459
Rafael Batista <RafaelB2424@gmail.com>
2021-03-10 23:47:32 -0500

    Fix enzyme adapter issue by renaming to setupTests.js

--- Client/src/setUpTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from "enzyme";
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import "@testing-library/jest-dom/extend-expect";
-import { enableFetchMocks } from "jest-fetch-mock";
-
-configure({ adapter: new Adapter() });
-enableFetchMocks();
