ae220f945fc69bc48388797b28beecfcaa335d5f
Raf-Batista <RafaelB2424@gmail.com>
2021-03-11 15:34:22 -0800

    Add more tests for user reducer

--- Client/src/state/__tests__/reducers.test.js
+++ Client/src/state/__tests__/reducers.test.js
@@ -1,7 +1,7 @@
 import userReducer from '../reducers/userReducer';
 import * as types from '../action-types';
 
-describe('bundles reducer', () => {
+describe('user reducer', () => {
     it('should return the initial state', () => {
         const initialUserState = {
             username: "",
@@ -10,5 +10,40 @@ describe('bundles reducer', () => {
           };
 
         expect(userReducer(undefined, {})).toEqual(initialUserState);
+    });
+
+    it('should add a user when checking if a user is logged in', () => {
+        const expectedResult = {
+            username: "test",
+            codes: [{ title: "one", order: ["a"], data: ["let a = 1;"] }, { title: "two", order: ["b"], data: ["let b = 2;"] }]
+          };
+
+        const payload = {
+            username: "test",
+            codes: [{ title: "one", order: ["a"], data: ["let a = 1;"] }, { title: "two", order: ["b"], data: ["let b = 2;"] }]
+        }
+
+        expect(userReducer({}, {type: types.ActionType.CHECK_IF_LOGGED_IN, payload: payload})).toEqual(expectedResult);
+    });
+
+    it('should set logging in to true ', () => {
+        const expectedResult = {isLoggingIn: true};
+
+        expect(userReducer({isLoggingIn: false}, {type: types.ActionType.LOGIN_USER_START})).toEqual(expectedResult);
+    });
+
+    it('should successfully add a use when logging in', () => {
+        const expectedResult = {
+            username: "test",
+            codes: [{ title: "one", order: ["a"], data: ["let a = 1;"] }, { title: "two", order: ["b"], data: ["let b = 2;"] }], 
+            isLoggingIn: false
+          };
+        
+        const payload = {
+            username: "test",
+            codes: [{ title: "one", order: ["a"], data: ["let a = 1;"] }, { title: "two", order: ["b"], data: ["let b = 2;"] }]
+        };
+
+        expect(userReducer({}, {type: types.ActionType.LOGIN_USER_COMPLETE, payload: payload})).toEqual(expectedResult);
     });
 });
